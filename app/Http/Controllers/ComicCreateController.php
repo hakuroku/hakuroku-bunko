b644fdfad7189b69bfc3519dfaa89d78906e5eaa
hakuroku <hakur0ku.ubu@gmail.com>
2024-11-13 16:04:35 +0900

    データーベース投稿プログラム編集

--- app/Http/Controllers/ComicCreateController.php
+++ app/Http/Controllers/ComicCreateController.php
@@ -10,19 +10,29 @@ class ComicCreateController extends Controller
 {
     public function create(Request $request)
     {
-
         $comic_title = $request->comic_title;
-        if ($request->hasFile('comic_content')) {
-
+        $series_title = $request->series_title;
+        $comic_content = $request->comic_content;
+        $author_name = $request->author_name;
+        // if ($request->hasFile('comic_content')) {
+            if (true) {
+            dump($request);
+            Storage::makeDirectory($comic_title);
+            $url = Storage::url('app');
             $file = $request->file('comic_content');
-            $path = Storage::disk('local')->putFile($comic_title, $file);
-            return response()->json(['message' => 'comic uploaded successfully', 'url' => Storage::disk('local')->url($path)]);
+            $path = Storage::disk('local')->putFile($url, $file, "1");
+            return response()->json(
+                [
+                    'message' => 'comic uploaded successfully', 
+                    'url' =>  $path
+                ]);
         }
+
         $post = Comic::create([
-            'comic_title' => $request->comic_title,
-            'series_title' => $request->series_title,
-            'comic_content' => $request->comic_content,
-            'author_name' => $request->author_name
+            'comic_title' => $comic_title,
+            'series_title' => $series_title,
+            'comic_content' => $comic_content,
+            'author_name' => $author_name
         ]);
 
         return view('comics.create');
